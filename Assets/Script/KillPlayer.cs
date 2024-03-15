3f10e65265d662be49019622d280ad522ce5944f
Shogun-th <kasidis.phum@bumail.net>
2024-03-15 21:01:55 +0700

    60%

---
+++ Assets/Script/KillPlayer.cs
@@ -0,0 +1,19 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class KillPlayer : MonoBehaviour
+{
+    public int Respawn;
+    
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            SceneManager.LoadScene(Respawn);
+        }
+    }
+}
--- Assets/Script/PlaneController.cs
+++ Assets/Script/PlaneController.cs
@@ -16,7 +16,7 @@ public class PlaneController : MonoBehaviour
     [Tooltip("How responsive the plane is when rolling, pitching , and yawing")]
     public float ressponsiveness = 10f;
 
-    
+
 
     private float throttle;
     private float roll;
@@ -24,47 +24,52 @@ public class PlaneController : MonoBehaviour
     private float yaw;
 
     private float responseModifier
-    { get { return (rb.mass / 10f) * ressponsiveness; } }
+    {
+        get { return (rb.mass / 10f) * ressponsiveness; }
+    }
 
-     Rigidbody rb;
-     [SerializeField]  TextMeshProUGUI hud;
-     private void Awake()
-     {
-         rb = GetComponent<Rigidbody>();
-     }
+    Rigidbody rb;
+    [SerializeField] TextMeshProUGUI hud;
 
-     private void HandleInputs()
-     {
-         roll = Input.GetAxis("Roll");
-         pitch = Input.GetAxis("Pitch");
-         yaw = Input.GetAxis("Yaw");
+    private void Awake()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
 
-         if (Input.GetKey(KeyCode.Space)) throttle += throttleIncrement;
-         else if (Input.GetKey(KeyCode.LeftControl)) throttle -= throttleIncrement;
-         throttle = Mathf.Clamp(throttle, 0f, 100f);
+    private void HandleInputs()
+    {
+        roll = Input.GetAxis("Roll");
+        pitch = Input.GetAxis("Pitch");
+        yaw = Input.GetAxis("Yaw");
 
-     }
+        if (Input.GetKey(KeyCode.Space)) throttle += throttleIncrement;
+        else if (Input.GetKey(KeyCode.LeftControl)) throttle -= throttleIncrement;
+        throttle = Mathf.Clamp(throttle, 0f, 100f);
 
-     private void Update()
-     {
-         HandleInputs();
-         UpdateHUD();
-     }
+    }
 
-     private void FixedUpdate()
-     {
-         rb.AddForce(transform.forward * maxThrust *throttle );
-         rb.AddTorque(transform.up * yaw * responseModifier);
-         rb.AddTorque(transform.right * pitch * responseModifier);
-         rb.AddTorque(-transform.forward * roll * responseModifier);
-         
-         
-     }
+    private void Update()
+    {
+        HandleInputs();
+        UpdateHUD();
+    }
 
-     private void UpdateHUD()
-     {
-         hud.text = "Throttle" + throttle.ToString("F0") + "%\n";
-         hud.text += "Airspeed:" + (rb.velocity.magnitude * 3.6f).ToString("F0") + "ku/h\n";
-         hud.text += "Altitude:" + transform.position.y.ToString("F0") + "m";
-     }
+    private void FixedUpdate()
+    {
+        rb.AddForce(transform.forward * maxThrust * throttle);
+        rb.AddTorque(transform.up * yaw * responseModifier);
+        rb.AddTorque(transform.right * pitch * responseModifier);
+        rb.AddTorque(-transform.forward * roll * responseModifier);
+
+
+    }
+
+    private void UpdateHUD()
+    {
+        hud.text = "Throttle" + throttle.ToString("F0") + "%\n";
+        hud.text += "Airspeed:" + (rb.velocity.magnitude * 3.6f).ToString("F0") + "ku/h\n";
+        hud.text += "Altitude:" + transform.position.y.ToString("F0") + "m";
+    }
+
+    
 }
